6d7041d8ba0ce4a9a64690b8cb1a59422ea997ab
takealook97 <takealook97@naver.com>
2023-10-24 22:52:19 +0900

    style: TokenProvider 주석 추가

--- src/main/java/com/jwt/practice/config/jwt/TokenProvider.java
+++ src/main/java/com/jwt/practice/config/jwt/TokenProvider.java
@@ -42,20 +42,20 @@ public class TokenProvider implements InitializingBean {
 	}
 
 	@Override
-	public void afterPropertiesSet() {
+	public void afterPropertiesSet() {// 빈생성, 의존성 주입 후 주입받은 secret값을 decode한 뒤 키 변수에 할당하기 위함
 		byte[] keyBytes = Decoders.BASE64.decode(secret);
 		this.key = Keys.hmacShaKeyFor(keyBytes);
 	}
 
-	public String createToken(Authentication authentication) {
+	public String createToken(Authentication authentication) {// Authentication 객체의 권한정보를 통해 토큰 생성
 		String authorities = authentication.getAuthorities().stream()
 			.map(GrantedAuthority::getAuthority)
-			.collect(Collectors.joining(","));
+			.collect(Collectors.joining(","));// 권한 가져오
 
 		long now = (new Date()).getTime();
-		Date validity = new Date(now + this.tokenValidityInMilliSeconds);
+		Date validity = new Date(now + this.tokenValidityInMilliSeconds);// 토큰 expire time 설정
 
-		return Jwts.builder()
+		return Jwts.builder()// jwt 토큰 생성 후 리턴
 			.setSubject(authentication.getName())
 			.claim(AUTHORITIES_KEY, authorities)
 			.signWith(SignatureAlgorithm.HS512, key)
@@ -63,26 +63,26 @@ public class TokenProvider implements InitializingBean {
 			.compact();
 	}
 
-	public Authentication getAuthentication(String token) {
-		Claims claims = Jwts.parser()
+	public Authentication getAuthentication(String token) {// 토큰에 담겨있는 정보를 통해 Authentication 객체 리턴
+		Claims claims = Jwts.parser()// 토큰을 통해 Claim 생성
 			.setSigningKey(key)
 			.parseClaimsJws(token)
 			.getBody();
 
-		Collection<? extends GrantedAuthority> authorities =
+		Collection<? extends GrantedAuthority> authorities =// claim에서 권한정보들을 가져온다.
 			Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
 				.map(SimpleGrantedAuthority::new)
 				.collect(Collectors.toList());
 
-		User principal = new User(claims.getSubject(), "", authorities);
+		User principal = new User(claims.getSubject(), "", authorities);// 유저 객체 생성
 
 		return new UsernamePasswordAuthenticationToken(principal, token, authorities);
 	}
 
-	public boolean validateToken(String token) {
+	public boolean validateToken(String token) {// 토큰 유효성 검사 메서드
 		try {
-			Jwts.parser().setSigningKey(key).parseClaimsJws(token);
-			return true;
+			Jwts.parser().setSigningKey(key).parseClaimsJws(token);// 파싱 시
+			return true;// 문제 없다면 true 리턴
 		} catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
 			logger.info("잘못된 JWT 서명입니다.");
 		} catch (ExpiredJwtException e) {
